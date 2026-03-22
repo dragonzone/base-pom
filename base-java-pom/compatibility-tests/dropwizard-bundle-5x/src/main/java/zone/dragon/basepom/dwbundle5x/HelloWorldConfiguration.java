6237efa0413c82906a8c6d30ecfaad7e72b20c97
Bryan Harclerode <baharclerode@devdragon.net>
2026-03-21 22:08:30 -0500

    Add support for dropwizard 5 to base poms

---
+++ base-java-pom/compatibility-tests/dropwizard-bundle-5x/src/main/java/zone/dragon/basepom/dwbundle5x/HelloWorldConfiguration.java
@@ -0,0 +1,18 @@
+package zone.dragon.basepom.dwbundle5x;
+
+import io.dropwizard.core.Configuration;
+import io.dropwizard.core.server.SimpleServerFactory;
+import io.dropwizard.jetty.HttpConnectorFactory;
+
+public class HelloWorldConfiguration extends Configuration {
+
+    public HelloWorldConfiguration() {
+        super();
+
+        SimpleServerFactory ssf = new SimpleServerFactory();
+        HttpConnectorFactory hcf = new HttpConnectorFactory();
+        hcf.setPort(0);
+        ssf.setConnector(hcf);
+        setServerFactory(ssf);
+    }
+}
---
+++ base-java-pom/compatibility-tests/dropwizard-bundle-5x/src/main/java/zone/dragon/basepom/dwbundle5x/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package zone.dragon.basepom.dwbundle5x;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+@Produces(MediaType.TEXT_PLAIN)
+public class HelloWorldResource {
+
+    @GET
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
