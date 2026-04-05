6f70939b5c5d8ae639111e321028c7b69c69eebf
Bryan Harclerode <baharclerode@devdragon.net>
2026-04-05 10:08:46 -0500

    Pin bytebuddy and ASM, even when using older versions of dependent libraries such as mockito

---
+++ base-java-pom/compatibility-tests/dropwizard-1x/src/test/java/zone/dragon/basepom/dw1x/MockitoCompatibilityTest.java
@@ -0,0 +1,25 @@
+package zone.dragon.basepom.dw1x;
+
+import java.util.HashSet;
+import java.util.function.Supplier;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MockitoCompatibilityTest {
+
+    @Test
+    public void mockitoCanMockJdkInterfaces() {
+        Supplier<?> mock = Mockito.mock(Supplier.class);
+        assertThat(mock).isNotNull();
+        assertThat(mock.get()).isNull();
+    }
+
+    @Test
+    public void mockitoCanMockJdkClasses() {
+        HashSet<?> mock = Mockito.mock(HashSet.class);
+        assertThat(mock).isNotNull();
+        assertThat(mock.size()).isEqualTo(0);
+    }
+}
